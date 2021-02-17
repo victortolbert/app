8b7d4dd914c7290bde0bf30017a063c1909d53e1
Victor Tolbert <victor.tolbert@gmail.com>
2021-02-17 07:15:20 -0500

    wip

--- pages/testing.vue
+++ pages/testing.vue
@@ -1,10 +1,3 @@
-<template>
-  <div>
-    <p>Our baseURL is: {{ $config.baseURL }}</p>
-    <p>Our assetsURL is: {{ $config.assetsURL }}</p>
-  </div>
-</template>
-
 <script>
 // const posts = await fetch(`${env.baseURL}/posts`)
 // const posts = await fetch(`${baseURL}/posts`)
@@ -16,4 +9,32 @@
 //     }
 //   },
 // }
+const MyList = {
+  template: `
+    <div>
+      <h1>My List</h1>
+      <ul><li v-for="item in list" :key="item">{{ item }}</li></ul>
+    </div>
+  `,
+  data: () => ({list: [1, 2, 3]}),
+}
+export default {
+  components: {MyList},
+  data: () => ({count: 0}),
+}
 </script>
+
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <MyList :class="$style.list" />
+    <p>Our baseURL is: {{ $config.baseURL }}</p>
+    <p>Our assetsURL is: {{ $config.assetsURL }}</p>
+  </div>
+</template>
+
+<style module>
+.list {
+  border: 1px solid red;
+}
+</style>
